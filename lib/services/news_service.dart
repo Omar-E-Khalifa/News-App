4b8ba3c554344b00c2cbfe5cda793f76c1013a48
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-07-03 02:00:49 +0300

    hide api key

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -3,12 +3,13 @@ import 'package:news_app/models/article_model.dart';
 
 class NewsService {
   final Dio dio;
+  final String apikey = const String.fromEnvironment('NEWS_API_KEY');
 
   NewsService(this.dio);
 
   Future<List<ArticleModel>> getNews({required String category}) async {
     final response = await dio.get(
-      'https://newsapi.org/v2/top-headlines?country=us&apikey=28717189a6414bf588c5298840cbaefe&category=$category',
+      'https://newsapi.org/v2/top-headlines?country=us&apikey=$apikey&category=$category',
     );
 
     Map<String, dynamic> jsonData = response.data;
